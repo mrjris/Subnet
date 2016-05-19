03f5e76a90587d5036e960dcf745c1a01f7ccb25
mrjris <mr.jris@yahoo.com>
2016-05-19 23:52:28 +0700

    Đổi tên

--- Subnet/Form1.Designer.cs
+++ Subnet/Form1.Designer.cs
@@ -30,37 +30,40 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
             this.txtIP = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.txtResult);
             this.groupBox1.Controls.Add(this.txtIP);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(8, 7);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(338, 94);
+            this.groupBox1.Size = new System.Drawing.Size(272, 107);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[MMT] Lê Văn Đạt";
             // 
-            // label1
+            // txtResult
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Địa chỉ IP";
+            this.txtResult.Location = new System.Drawing.Point(70, 51);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(181, 20);
+            this.txtResult.TabIndex = 1;
             // 
             // txtIP
             // 
-            this.txtIP.Location = new System.Drawing.Point(151, 22);
+            this.txtIP.Location = new System.Drawing.Point(70, 22);
             this.txtIP.Name = "txtIP";
             this.txtIP.Size = new System.Drawing.Size(181, 20);
             this.txtIP.TabIndex = 1;
@@ -72,24 +75,36 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(6, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.Size = new System.Drawing.Size(41, 13);
             this.label2.TabIndex = 0;
-            this.label2.Text = "Lớp mạng của địa chỉ IP";
+            this.label2.Text = "Subnet";
             // 
-            // txtResult
+            // label1
             // 
-            this.txtResult.Location = new System.Drawing.Point(151, 51);
-            this.txtResult.Name = "txtResult";
-            this.txtResult.ReadOnly = true;
-            this.txtResult.Size = new System.Drawing.Size(181, 20);
-            this.txtResult.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(231, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập IP Address rồi nhấn Enter để tính Subnet";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 111);
+            this.ClientSize = new System.Drawing.Size(272, 107);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Subnet Calculator";
@@ -106,6 +121,7 @@
         private System.Windows.Forms.TextBox txtIP;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
