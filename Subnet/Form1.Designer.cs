a60fec17a9b952cb53e5e02820ab71254da8b56e
mrjris <mr.jris@yahoo.com>
2016-05-19 23:36:12 +0700

    xong

--- Subnet/Form1.Designer.cs
+++ Subnet/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtResult);
+            this.groupBox1.Controls.Add(this.txtIP);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(338, 94);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Địa chỉ IP";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(151, 22);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(181, 20);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "127.16.1.129/25";
+            this.txtIP.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Lớp mạng của địa chỉ IP";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(151, 51);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(181, 20);
+            this.txtResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(359, 111);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Subnet Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Subnet/Form1.cs
+++ Subnet/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +17,70 @@ namespace Subnet
         {
             InitializeComponent();
         }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Nếu gõ enter thì làm việc nếu không thì thôi
+            if (e.KeyCode != Keys.Enter) return;
+            //Lấy và tách dữ liệu
+            string s = txtIP.Text;
+            string[] dim = { ",", ".", " ", "\t", "\r\t", "/" };
+            string[] words = s.Split(dim, StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length != 5)
+            {
+                MessageBox.Show("Mời nhập đúng dạng Ví dụ: 127.16.1.129/25");
+                return;
+            }
+            //Thuật toán
+            //B1: Biến đổi IP về dạng binary 
+            string IP = "";
+            for (int i = 0; i< 4; i++)
+                IP += Convert.ToString(Convert.ToInt32(words[i]),2).PadLeft(8,'0');
+            //B2: Tìm subnetmask
+            int count = Convert.ToInt32(words[4]);
+            string subnet = "";
+            subnet = subnet.PadLeft(count, '1') + subnet.PadRight(32 - count, '0');
+            //B3: Dùng thuật toán AND giữa địa chỉ IP và subnetmask để ra lớp mạng.
+            string result = "";
+            for (int i = 0; i < 32; i++)
+                if (subnet[i] == '1' && IP[i] == '1') result += "1";
+                else result += "0";
+
+            result = ConvertBinaryToIp(result) + "/" + count.ToString();
+           
+            txtResult.Text = result;
+
+
+        }
+        static string ConvertBinaryToIp(string str32BitBinaryIP)
+
+        {
+            //Copy code trên stacoverflow
+
+            string[] strEach8BitBinaryIp = new string[4];
+            var split = str32BitBinaryIP.Select((c, index) => new { c, index })
+                .GroupBy(x => x.index / 8)
+                .Select(group => group.Select(elem => elem.c))
+                .Select(chars => new string(chars.ToArray()));
+            int n = 0;
+            foreach (var str in split)
+                strEach8BitBinaryIp[n++] = str.ToString();
+
+            int decimalValueOf8bitIp = 0;
+
+            string ipAddress = "";
+
+            for (int i = 0; i < strEach8BitBinaryIp.Length; i++)
+            {
+                decimalValueOf8bitIp = Convert.ToInt32(strEach8BitBinaryIp[i]);
+                ipAddress += Convert.ToInt32(decimalValueOf8bitIp.ToString(),2).ToString() + ".";
+            }//end of for
+
+
+            return ipAddress.Substring(0, ipAddress.Length - 1);
+
+
+        }// end of ConvertBinaryToIp() method
     }
 }
